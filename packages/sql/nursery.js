3431ccb9598500ab522785bdf62a11b8fca8cbef
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2025-07-27 16:45:17 -0400

    fix: use oxc + dprint

--- packages/sql/nursery.js
+++ packages/sql/nursery.js
@@ -6,7 +6,7 @@ setup({
   name: 'sql',
   treeSitterPackage: '@derekstride/tree-sitter-sql',
   languageRegistration,
-  testRunner: parse => {
+  testRunner: _parse => {
     // add test here
   },
 })
