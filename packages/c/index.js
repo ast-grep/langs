d8029d2cf3e357818e2848b7bd58795c44cad165
macabeus <bruno.macabeus@gmail.com>
2025-08-12 00:17:54 +0100

    fix bundling for other archs

--- packages/c/index.js
+++ packages/c/index.js
@@ -1,5 +1,30 @@
 const path = require('node:path')
-const libPath = path.join(__dirname, 'parser.so')
+
+function getNativePath() {
+  if (process.platform === 'win32') {
+    if (process.arch === 'x64') {
+      return 'prebuilds/prebuild-Windows-X64/parser.so'
+    }
+
+    throw new Error(`Unsupported architecture on Windows: ${process.arch}`)
+  } else if (process.platform === 'darwin') {
+    if (process.arch === 'arm64') {
+      return 'prebuilds/prebuild-macOS-ARM64/parser.so';
+    }
+
+    throw new Error(`Unsupported architecture on macOS: ${process.arch}`)
+  } else if (process.platform === 'linux') {
+    if (process.arch === 'x64') {
+      return 'prebuilds/prebuild-Linux-X64/parser.so';
+    }
+
+    throw new Error(`Unsupported architecture on Linux: ${process.arch}`)
+  }
+
+  throw new Error(`Unsupported OS: ${process.platform}, architecture: ${process.arch}`)
+}
+
+const libPath = path.join(__dirname, getNativePath())
 
 module.exports = {
   libraryPath: libPath,
