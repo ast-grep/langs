d18e8d979838899459cdc0e7b2762c2dc086eff3
BobrImperator <bartholomew.dudzik@gmail.com>
2026-01-15 14:53:53 +0100

    feat: add ember glimmer language parser (.gjs,.gts) extensions

---
+++ packages/glimmer-javascript/nursery.js
@@ -0,0 +1,49 @@
+const { setup } = require('@ast-grep/nursery')
+const assert = require('node:assert')
+const languageRegistration = require('./index')
+
+setup({
+  dirname: __dirname,
+  name: 'glimmer-javascript',
+  treeSitterPackage: 'tree-sitter-glimmer-javascript',
+  languageRegistration,
+  testRunner: (parse) => {
+    const sg = parse(`
+import Component from '@glimmer/component';
+
+function isLatestVersion() {
+  const stableRegex = /^d+.d+.d+$/;
+  return !stableRegex.test(VERSION);
+}
+
+export default class WelcomePageComponent extends Component {
+  <template>
+    <main id="ember-welcome-page-id-selector">
+      <Hello> </Hello>
+    </main>
+  </template>
+
+  @service session;
+
+  get extension() {
+    return this.args.extension ?? 'hbs';
+  }
+}
+  `);
+    const root = sg.root()
+
+    const getter = root.find({
+      rule: {
+        kind: 'method_definition'
+      }
+    });
+    const property = getter.find({ rule: { kind: 'property_identifier' } });
+    assert.equal(property.text(), 'extension')
+
+    const service = root.find({ rule: { kind: 'field_definition > property_identifier' } });
+    assert.equal(service.text(), 'session');
+  
+    const main_tag = root.find({ rule: { kind: 'glimmer_template > glimmer_opening_tag' } });
+    assert.equal(main_tag.text(), '<template>');
+  }
+})
